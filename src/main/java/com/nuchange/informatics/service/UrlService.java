1699ebf09a7ccbc81f540c313dd0b5147c0dc25a
Venkat Naga Raju Chinthanippu <69348444+Venkat-C-J@users.noreply.github.com>
2021-07-31 20:17:31 +0530

    Add files via upload
    removed the helper method for getting the url

--- src/main/java/com/nuchange/informatics/service/UrlService.java
+++ src/main/java/com/nuchange/informatics/service/UrlService.java
@@ -7,9 +7,7 @@ import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -38,34 +36,6 @@ public class UrlService {
 		entitymanager = theEntitymanager;
 	}
 
-	//Helper method to get the UrlEntity by url
-	public Optional<UrlEntity> getUrl(String theUrl) {
-
-		//get current hibernate session
-		Session currentSession =
-				entitymanager.unwrap(Session.class);
-
-		//get the url by imposing the Criteria
-		@SuppressWarnings("deprecation")
-		Criteria criteria = currentSession.createCriteria(UrlEntity.class)
-				.add(Restrictions.eq("url", theUrl));
-
-		// Convenience method to return a single instance that matches
-		// the query, or null if the query returns no results.
-		Object result = criteria.uniqueResult();
-		
-		//if result is not null then return the result by converting to URL
-		if(result != null) {
-			
-			UrlEntity url = (UrlEntity) result;
-			
-			return Optional.of(url);
-		}
-		
-		return Optional.empty();
-
-	}
-
 	public List<UrlEntity> getAllUrls(Integer pageNo, Integer pageSize) {
 		
 		//create the Pageable object to pass as a parameter to findAll method
@@ -89,6 +59,7 @@ public class UrlService {
 
 		//call the findAll method with above Pageable object which will return Page<UrlEntity>
 		Page<UrlEntity> pagedResult = repository.findAll(paging);
+		
 
 		//return the results
 		if(pagedResult.hasContent()) {
@@ -101,7 +72,7 @@ public class UrlService {
 	//method to save or update the url
 	public UrlEntity createOrUpdateUrl(UrlEntity entity) {
 		
-		Optional<UrlEntity> url = getUrl(entity.getUrl());
+		Optional<UrlEntity> url = repository.findByUrl(entity.getUrl());
 
 		//save the url if url does not exit and return the url
 		if(! url.isPresent()) {
@@ -112,12 +83,22 @@ public class UrlService {
 			
 		}
 		
-		//return the existing url
-		UrlEntity newEntity = url.get();
-		newEntity.setUrl(entity.getUrl());
-		newEntity.setUsageCount(entity.getUsageCount());
+		/*
+		 * if url exists and the usage count of the url is not equal 
+		 * to the usage count in DB save the url
+		 */
+		if((url.get().getUsageCount() != entity.getUsageCount()) && url.isPresent()) {
+			
+			UrlEntity newEntity = url.get();
+            newEntity.setUrl(entity.getUrl());
+            newEntity.setUsageCount(entity.getUsageCount());
+            newEntity = repository.save(newEntity);
+             
+            return newEntity;
+		}
 		
-		return newEntity;
+		//else return the url from DB
+		return url.get();
 	}
 
 	//method to get the unique short key of the url
@@ -129,7 +110,7 @@ public class UrlService {
 				entitymanager.unwrap(Session.class);
 
 		//get the url
-		Optional<UrlEntity> url = getUrl(theUrl);
+		Optional<UrlEntity> url = repository.findByUrl(theUrl);
 
 		//if url exist return the unique short key after incrementing the usage count.
 		if(url.isPresent()) {
@@ -153,7 +134,7 @@ public class UrlService {
 	public int getUrlUsageCount(String theUrl) throws RecordNotFoundException{
 		
 		//get the url and return the usage count
-		Optional<UrlEntity> url = getUrl(theUrl);
+		Optional<UrlEntity> url = repository.findByUrl(theUrl);
 		
 		if(url.isPresent()) {
 			return url.get().getUsageCount();
@@ -165,7 +146,7 @@ public class UrlService {
 	public UrlEntity delete(String theUrl) throws RecordNotFoundException{
 		
 		//get the url
-		Optional<UrlEntity> url = getUrl(theUrl);
+		Optional<UrlEntity> url = repository.findByUrl(theUrl);
 		
 		//if url is present delete the url
 		if(url.isPresent()) {
