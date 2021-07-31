5835a2a166593801228fc2930d76c9f16a1ac230
Venkat Naga Raju Chinthanippu <69348444+Venkat-C-J@users.noreply.github.com>
2021-07-31 20:19:21 +0530

    Add files via upload
    added @Query to get the record by url

--- src/main/java/com/nuchange/informatics/repository/UrlRepository.java
+++ src/main/java/com/nuchange/informatics/repository/UrlRepository.java
@@ -2,7 +2,11 @@ package com.nuchange.informatics.repository;
 
 
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.nuchange.informatics.model.UrlEntity;
@@ -11,4 +15,16 @@ import com.nuchange.informatics.model.UrlEntity;
 public interface UrlRepository extends PagingAndSortingRepository<UrlEntity, Integer>{
 
 
+	/*
+	 * Using method Optional<UrlEntity> findByEmployeeName(@Param("name") String
+	 * name);
+	 * 
+	 * @Query(value = "SELECT u from UrlEntity u where u.url =:url ", nativeQuery =
+	 * true) @ using @query with native Optional<UrlEntity> findByUrl(@Param("url")
+	 * String url);
+	 */
+	
+    @Query("SELECT u from UrlEntity u where u.url =:url ")
+    Optional<UrlEntity> findByUrl(@Param("url") String url);
+    
 }
